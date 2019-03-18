19b92ea5dafb5e8463dcf632c6bb9bd7a8138441
liangzr <liangzr@outlook.com>
2019-03-18 22:22:39 +0800

    ⚡️  Optimize the notify strategy

--- notify.sh
+++ notify.sh
@@ -1,13 +1,29 @@
 #! /usr/bin/env sh
 
+# GitHub didn't update the contributions graph immediately, but
+# only the commits from default branch can be display on the
+# contributions graph. So we can change the default branch to
+# force it refresh
+
+# Wait a moment
+sleep 60
+
 curl https://api.github.com/repos/${GITHUB_USER}/github-run \
   -u ${GITHUB_USER}:${GITHUB_DEV_TOKEN} \
   -X PATCH \
   -H 'Content-Type: application/json;charset=UTF-8' \
-  -d '{"name":"github-run","default_branch":"master"}'
+  -d '{"name":"github-run","default_branch":"master"}' \
+  > /dev/null -s
+  
+echo 'Change default branch to master'
+# curl https://github.com/${GITHUB_USER} > /dev/null -s
   
 curl https://api.github.com/repos/${GITHUB_USER}/github-run \
   -u ${GITHUB_USER}:${GITHUB_DEV_TOKEN} \
   -X PATCH \
   -H 'Content-Type: application/json;charset=UTF-8' \
-  -d '{"name":"github-run","default_branch":"graph"}'
+  -d '{"name":"github-run","default_branch":"graph"}' \
+  > /dev/null -s
+
+echo 'Default branch change back to graph'
+echo 'Notified the GitHub'
